4ce3b9c60f524a7e47bf9e2b6d11a46dc144e039
Tsugami <yslan3000@gmail.com>
2020-05-22 15:22:37 -0300

    add assert pawn movement

--- src/errors.js
+++ src/errors.js
@@ -1,3 +1,7 @@
 module.exports = {
-  squareEmply: (row, column) => `There are no pieces in row ${row} column ${column}`
+  invalidMovement: 'INVALID_MOVEMNT',
+  invalidRow: 'INVALID_ROW: Row cannot be greater than 8 or less than 1.',
+  invalidColumn: 'INVALID_COLUMN: Column cannot be greater than 8 or less than 1.',
+  squareEmply: (row, column) => `SQUARE_EMPLY: There are no pieces in row ${row} column ${column}`,
+  pawnInvalidMovemnt: 'PAWN_INVALID_MOVEMNT: The pawn can only make moves adjacent to its previous position, that is, it cannot move backwards. The pawn, like the king, can only move 1 square ahead per move, however, when the pawn is still in its starting position, it can jump 2 spaces ahead.'
 }
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ class ChessCanvas {
     capturedColor = '#ff0000',
     pieceWidth = 50,
     pieceHeigth = 50,
+    initialTurn = 'white',
     squareWhiteColor = '#fff',
     squareBlackColor = '#B8681A',
   } = {}) {
@@ -19,6 +20,7 @@ class ChessCanvas {
     this.canvasContext = null;
     this.pieceImages = null;
 
+    this.turn = initialTurn;
     this.moveColor = moveColor;
     this.capturedColor = capturedColor;
     this.pieceWidth = pieceWidth;
@@ -38,6 +40,12 @@ class ChessCanvas {
     this.whitePawnImage = null;
     this.whiteQueenImage = null;
     this.whiteRookImage = null;
+    this.lastMovemnt = {
+      startRow: null,
+      startColumn: null,
+      endColumn: null,
+      endRow: null,
+    }
   }
 
   get boardWidth () {
@@ -48,8 +56,50 @@ class ChessCanvas {
     return this.pieceHeigth * 8;
   }
 
-  findPiece (row, column) {
-    return this.pieces.find(c => c.row === row && c.column === column);
+  capturePiece (row, column, turn = this.turn) {
+    const i = this.pieces.findIndex(
+      this._filter(
+        row,
+        column,
+        this.getNextTurn(turn)
+    ));
+    if (i < 0) return false;
+    this.pieces.splice(i, 1);
+    return true
+  }
+
+  getNextTurn (turn = this.turn) {
+    return turn === 'white' ? 'black' : 'white' 
+  } 
+
+  _filter (row, column, search) {
+    return (pieceData) => 
+    pieceData.row === row &&
+    pieceData.column === column &&
+    pieceData.piece.startsWith(search)
+  }
+
+  findPiece (row, column, turn = this.turn) {
+    return this.pieces.find(this._filter(row, column, turn));
+  }
+
+  movePiece (piece, startRow, startColumn, endRow, endColumn, turn = this.turn) {
+    this.pieces.splice(
+      this.pieces.findIndex(
+        this._filter(
+          startRow,
+          startColumn,
+          this.getNextTurn(turn)
+        )
+      ),
+      1,
+      {
+        piece,
+        row: endRow,
+        column: endColumn,
+        isInitialPosition: false
+      }
+    )
   }
 
   addPiece (piece, row, column, isInitialPosition = false) {
@@ -61,10 +111,9 @@ class ChessCanvas {
     this.canvasContext = this.canvas.getContext('2d');
     this.pieceImages = await this.loadPieceImages();
 
-    this._drawSquares();
     this._setInitialPieces('white', 2, 1);
     this._setInitialPieces('black', 7, 8);
-    this._drawInitialPieces();
+    this.render();
   }
 
   _setInitialPieces (color, frontRow, backRow) {
@@ -89,7 +138,7 @@ class ChessCanvas {
         y += 1;
         x = 0;
       }
-      const squareColor = (i + y) % 2 ? this.squareWhiteColor : this.squareBlackColor;
+      const squareColor = this._getSquareColor(x, y);
       const squareX = x * this.pieceWidth;
       const squareY = y * this.pieceHeigth;
       this._drawSquare(squareColor, squareX, squareY)
@@ -97,12 +146,17 @@ class ChessCanvas {
     }
   }
 
+  _getSquareColor (row, column) {
+    return (column + row) % 2 ? this.squareWhiteColor : this.squareBlackColor;
+  }
+
   _drawSquare (color, x, y) {
     this.canvasContext.fillStyle = color;
     this.canvasContext.fillRect(x, y, this.pieceWidth, this.pieceHeigth);
   }
 
-  _drawInitialPieces () {
+  render () {
+    this._drawSquares();
     for (const { piece, column, row } of this.pieces) {
       this._drawPiece(
         this._getPieceImage(piece),
@@ -154,6 +208,128 @@ class ChessCanvas {
     this._drawSquare(color, this._getX(startColumn), this._getY(startRow));
     this._drawSquare(color, this._getX(endColumn), this._getY(endRow));
     this._drawPiece(pieceImage, this._getX(endColumn), this._getY(endRow));
+  }
+
+  _cleanLastMovemnt () {
+    const {
+      startRow,
+      startColumn,
+      endRow,
+      endColumn,
+    } = this.lastMovemnt;
+
+    if (
+      !startRow ||
+      !startColumn ||
+      !endRow ||
+      !endColumn
+      ) return;
+    console.log(
+      this._getSquareColor(startRow, startColumn),
+      this._getX(startRow),
+      this._getX(startColumn),
+      );
+    this._drawSquare(
+      this._getSquareColor(startRow, startColumn),
+      this._getX(startRow),
+      this._getX(startColumn));
+    
+    this._drawSquare(
+      this._getSquareColor(endRow, endColumn),
+      this._getX(endRow),
+      this._getX(endColumn));
+  }
+
+  move (startRow, startColumn, endRow, endColumn) {
+    this._cleanLastMovemnt();
+
+    const piece = this.findPiece(startRow, startColumn);
+    if (!piece) {
+      throw new Error(errors.squareEmply(startRow, startColumn));
+    }
+    const pieceName = piece.piece.replace(/black|white/i, '')
+    const captured = this.capturePiece(endRow, endColumn);
+    this.assertMovement(
+      pieceName,
+      startRow,
+      startColumn,
+      endRow,
+      endColumn,
+      captured,
+      this.turn === 'white',
+      piece.isInitialPosition,
+    );
+
+    this.movePiece(
+      piece.piece,
+      startRow,
+      startColumn,
+      endRow,
+      endColumn,
+      this.turn,
+      );
+    
+    this._drawMovement(
+      this._getPieceImage(piece.piece),
+      startRow, startColumn, endRow, endColumn, captured
+    )
+
+    this.lastMovemnt = {
+      startRow,
+      startColumn,
+      endRow,
+      endColumn,
+    }
+
+    this.turn = this.getNextTurn(this.turn);
+  }
+
+  assertMovement (
+    pieceName,
+    startRow,
+    startColumn,
+    endRow,
+    endColumn,
+    captured = false,
+    isWhiteTurn = true,
+    isInitialPosition = false,
+  ) {
+    const isSamePoistion = startRow === endRow && startColumn === endColumn;
+    if (isSamePoistion) throw new Error(errors.invalidMovement);
+    this.assertPosition(startRow, startColumn);
+    this.assertPosition(endRow, endColumn);
+
+    switch (pieceName) {
+      case 'Pawn': {
+        const isWhiteBackMove = isWhiteTurn && startRow > endRow;
+        const isBlackBackMove = !isWhiteTurn && startRow < endRow;
+        const isDifferentColumn = startColumn !== endColumn;
+        const isWhiteDiagonalMove = isDifferentColumn && endRow !== (startRow + 1);
+        const isBlackDiagonalMove = isDifferentColumn && endRow !== (startRow - 1);
+        const isDiagonalMoveWithoutCapture = !captured && (isWhiteDiagonalMove || isBlackDiagonalMove);
+        const isMoreTwoSquares = isWhiteTurn && endRow > (startRow + 2);
+        const isWhiteTwoSquaresOutInitPos = isWhiteTurn && endRow === (startRow + 2) && !isInitialPosition;
+        const isBlackTwoSquaresOutInitPos = !isWhiteTurn && endRow === (startRow - 2) && !isInitialPosition;
+        if (
+          isWhiteBackMove ||
+          isBlackBackMove ||
+          isDiagonalMoveWithoutCapture ||
+          isMoreTwoSquares ||
+          isWhiteTwoSquaresOutInitPos ||
+          isBlackTwoSquaresOutInitPos
+        ) {
+          throw new Error(errors.pawnInvalidMovemnt)
+        }
+        break;
+      }
+    }
+  }
+
+  assertPosition (row, column) {
+    const validate = x => x > 8 || x < 1;
+
+    if (validate(row)) throw new Error(errors.invalidRow);
+    if (validate(column)) throw new Error(errors.invalidColumn);
   }
 }
 
